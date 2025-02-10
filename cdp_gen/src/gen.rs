4675d864b435778dc406088ec3ecc6b14225780c
Tommy Gilligan <tommy.gilligan@arkeus.com>
2025-02-10 17:28:23 +1100

    add more naming exceptions closes #13

--- cdp_gen/src/gen.rs
+++ cdp_gen/src/gen.rs
@@ -15,23 +15,22 @@ fn parse_ref(p: &crate::parser::Parameter) -> Option<String> {
     }
 }
 
-use regex::{Regex, Captures};
+use regex::{Captures, Regex};
 
 fn process_doc(s: String) -> String {
     let re = Regex::new(r"(\s)(<[^>]+>)").unwrap();
-    let a = re.replace_all(&s, |caps: &Captures| {
-        format!("{}`{}`", &caps[1], &caps[2])
-    }).into_owned();
+    let a = re
+        .replace_all(&s, |caps: &Captures| format!("{}`{}`", &caps[1], &caps[2]))
+        .into_owned();
 
     let re = Regex::new(r"(https?://\S+)").unwrap();
-    let b = re.replace_all(&a, |caps: &Captures| {
-        format!("<{}>", &caps[1])
-    }).into_owned();
+    let b = re
+        .replace_all(&a, |caps: &Captures| format!("<{}>", &caps[1]))
+        .into_owned();
 
     let re = Regex::new(r"(\[|\])").unwrap();
-    re.replace_all(&b, |caps: &Captures| {
-        format!("\\{}", &caps[1])
-    }).into_owned()
+    re.replace_all(&b, |caps: &Captures| format!("\\{}", &caps[1]))
+        .into_owned()
 }
 
 fn to_type(p: &crate::parser::Parameter, self_referential: bool) -> Option<(codegen::Type, bool)> {
@@ -74,7 +73,9 @@ fn to_type(p: &crate::parser::Parameter, self_referential: bool) -> Option<(code
         Some(_s) if p.r#type.clone() == Some("number".to_owned()) => Some("f64".to_owned()),
         Some(_s) if p.r#type.clone() == Some("boolean".to_owned()) => Some("bool".to_owned()),
         Some(_s) if p.r#type.clone() == Some("any".to_owned()) => Some("()".to_owned()),
-        Some(_s) if p.r#type.clone() == Some("object".to_owned()) => Some("serde_json::value::Value".to_owned()),
+        Some(_s) if p.r#type.clone() == Some("object".to_owned()) => {
+            Some("serde_json::value::Value".to_owned())
+        }
         Some(s) => panic!("{:?}", s),
         _ => None,
     };
@@ -130,7 +131,8 @@ fn variant_name(name: String) -> String {
 }
 
 pub fn field_name(name: String) -> String {
-    match name.clone().as_str() {
+    let tr = match name.clone().as_str() {
+        "backendDOMNodeId" => "backend_dom_node_id".to_owned(),
         "DOM" => "dom".to_owned(),
         "DOMDebugger" => "dom_debugger".to_owned(),
         "DOMSnapshot" => "dom_snapshot".to_owned(),
@@ -139,6 +141,63 @@ pub fn field_name(name: String) -> String {
         "PWA" => "pwa".to_owned(),
         "IndexedDB" => "indexed_db".to_owned(),
         "CSS" => "css".to_owned(),
+        "printToPDF" => "print_to_pdf".to_owned(),
+        "setBypassCSP" => "set_bypass_csp".to_owned(),
+        "setCPUThrottlingRate" => "set_cpu_throttling_rate".to_owned(),
+        "setXHRBreakpoint" => "set_xhr_breakpoint".to_owned(),
+        "setShowFPSCounter" => "set_show_fps_counter".to_owned(),
+        "getDOMCountersForLeakDetection" => "get_dom_counters_for_leak_detection".to_owned(),
+        "getDOMCounters" => "get_dom_counters".to_owned(),
+        "getDOMStorageItems" => "get_dom_storage_items".to_owned(),
+        "getOuterHTML" => "get_outer_html".to_owned(),
+        "removeXHRBreakpoint" => "remove_xhr_breakpoint".to_owned(),
+        "trackIndexedDBForOrigin" => "track_indexed_db_for_origin".to_owned(),
+        "trackIndexedDBForStorageKey" => "track_indexed_db_for_storage_key".to_owned(),
+        "setOuterHTML" => "set_outer_html".to_owned(),
+        "setExtraHTTPHeaders" => "set_extra_http_headers".to_owned(),
+        "baseURL" => "base_url".to_owned(),
+        "blockedURL" => "blocked_url".to_owned(),
+        "currentSourceURL" => "current_source_url".to_owned(),
+        "dataURL" => "data_url".to_owned(),
+        "documentURL" => "document_url".to_owned(),
+        "enableUI" => "enable_ui".to_owned(),
+        "externalURL" => "external_url".to_owned(),
+        "gatedAPIFeatures" => "gated_api_features".to_owned(),
+        "hasSourceURL" => "has_source_url".to_owned(),
+        "includeCommandLineAPI" => "include_command_line_api".to_owned(),
+        "includeDOMRects" => "include_dom_rects".to_owned(),
+        "includeObjectsCollectedByMajorGC" => "include_objects_collected_by_major_gc".to_owned(),
+        "includeObjectsCollectedByMinorGC" => "include_objects_collected_by_minor_gc".to_owned(),
+        "includeUserAgentShadowDOM" => "include_user_agent_shadow_dom".to_owned(),
+        "initiatorIPAddressSpace" => "initiator_ip_address_space".to_owned(),
+        "insecureURL" => "insecure_url".to_owned(),
+        "isBadUP" => "is_bad_up".to_owned(),
+        "isBadUV" => "is_bad_uv".to_owned(),
+        "isSVG" => "is_svg".to_owned(),
+        "mainResourceURL" => "main_resource_url".to_owned(),
+        "matchedCSSRules" => "matched_css_rules".to_owned(),
+        "modernSSL" => "modern_ssl".to_owned(),
+        "originURL" => "origin_url".to_owned(),
+        "outerHTML" => "outer_html".to_owned(),
+        "preferCSSPageSize" => "prefer_css_page_size".to_owned(),
+        "remoteIPAddress" => "remote_ip_address".to_owned(),
+        "removeDOMBreakpoint" => "remove_dom_breakpoint".to_owned(),
+        "removeDOMStorageItem" => "remove_dom_storage_item".to_owned(),
+        "reportAAA" => "report_aaa".to_owned(),
+        "requestURL" => "request_url".to_owned(),
+        "resourceIPAddressSpace" => "resource_ip_address_space".to_owned(),
+        "scopeURL" => "scope_url".to_owned(),
+        "scriptURL" => "script_url".to_owned(),
+        "setDOMBreakpoint" => "set_dom_breakpoint".to_owned(),
+        "setDOMStorageItem" => "set_dom_storage_item".to_owned(),
+        "showCSS" => "show_css".to_owned(),
+        "sourceMapURL" => "source_map_url".to_owned(),
+        "sourceURL" => "source_url".to_owned(),
+        "thresholdAA" => "threshold_aa".to_owned(),
+        "thresholdAAA" => "threshold_aaa".to_owned(),
+        "untrackIndexedDBForOrigin" => "untrack_indexed_db_for_origin".to_owned(),
+        "untrackIndexedDBForStorageKey" => "untrack_indexed_db_for_storage_key".to_owned(),
+        "userTypedURL" => "user_typed_url".to_owned(),
         _ => {
             let mut result = String::new();
             for (index, c) in name.chars().enumerate() {
@@ -155,7 +214,12 @@ pub fn field_name(name: String) -> String {
             }
             result
         }
-    }
+    };
+
+    println!("{}", name);
+    println!("{}", tr);
+
+    tr
 }
 
 pub fn main_client(domains: &[crate::parser::Domain]) -> Scope {
@@ -166,7 +230,6 @@ pub fn main_client(domains: &[crate::parser::Domain]) -> Scope {
     for domain in domains {
         if let Some(true) = domain.experimental {
         } else {
-
             let function = new_trait.new_fn(field_name(domain.domain.to_owned()));
             function.arg_mut_self();
 
@@ -368,7 +431,10 @@ pub fn r#gen(domain: crate::parser::Domain) -> Scope {
                     if let Some((c, _)) = to_type(a_return, false) {
                         r.new_field(field_name(escape_field_name(a_return.name.clone())), c)
                             .vis("pub")
-                            .annotation(format!("#[serde(rename = \"{}\")]", a_return.name.clone()));
+                            .annotation(format!(
+                                "#[serde(rename = \"{}\")]",
+                                a_return.name.clone()
+                            ));
                     }
                 }
 
@@ -398,13 +464,13 @@ pub fn r#gen(domain: crate::parser::Domain) -> Scope {
     }
 
     let client = module.new_struct("Client");
-    client.new_field("inner", "&'a mut T")
-    .vis("pub");
+    client.new_field("inner", "&'a mut T").vis("pub");
 
-    client.vis("pub")
-    .generic("'a")
-    .generic("T")
-    .bound("T", "crate::Client");
+    client
+        .vis("pub")
+        .generic("'a")
+        .generic("T")
+        .bound("T", "crate::Client");
 
     let client_impl = module.new_impl("Client");
     client_impl.target_generic("'_");
@@ -419,7 +485,10 @@ pub fn r#gen(domain: crate::parser::Domain) -> Scope {
             function.vis("pub");
             function.arg_mut_self();
             function.set_async(true);
-            function.ret(format!("Result<{}Return, (isize, String)>", variant_name(t.name.clone())));
+            function.ret(format!(
+                "Result<{}Return, (isize, String)>",
+                variant_name(t.name.clone())
+            ));
 
             if let Some(description) = &t.description {
                 function.doc(process_doc(description.clone()));
@@ -497,28 +566,228 @@ pub fn r#gen(domain: crate::parser::Domain) -> Scope {
         }
     }
 
-    let g = module.new_enum(variant_name("Event".to_owned()))
-            .derive("Debug")
-            .derive("PartialEq")
-            .derive("crate::Deserialize")
-            .vis("pub")
-            .r#macro("#[serde(tag = \"method\", content = \"params\")]");
+    let g = module
+        .new_enum(variant_name("Event".to_owned()))
+        .derive("Debug")
+        .derive("PartialEq")
+        .derive("crate::Deserialize")
+        .vis("pub")
+        .r#macro("#[serde(tag = \"method\", content = \"params\")]");
 
     if let Some(ref events) = domain.events {
         for t in events {
             if let Some(true) = t.experimental {
             } else {
-                let variant = g.new_variant(escape_field_name(variant_name(escape(t.name.to_string()))));
+                let variant =
+                    g.new_variant(escape_field_name(variant_name(escape(t.name.to_string()))));
                 if let Some(description) = &t.description {
                     for line in description.lines() {
                         variant.annotation(format!("/// {}", line));
                     }
                 }
-                    variant.annotation(format!("#[serde(rename = \"{}.{}\")]", domain.domain, t.name))
+                variant
+                    .annotation(format!(
+                        "#[serde(rename = \"{}.{}\")]",
+                        domain.domain, t.name
+                    ))
                     .tuple(variant_name(t.name.clone()));
             }
         }
     }
 
     module
+}
+
+#[test]
+fn test_field_name() {
+    assert_eq!(
+        field_name("getDOMCountersForLeakDetection".to_owned()),
+        "get_dom_counters_for_leak_detection".to_owned()
+    );
+    assert_eq!(
+        field_name("getDOMCounters".to_owned()),
+        "get_dom_counters".to_owned()
+    );
+    assert_eq!(
+        field_name("getDOMStorageItems".to_owned()),
+        "get_dom_storage_items".to_owned()
+    );
+    assert_eq!(
+        field_name("getOuterHTML".to_owned()),
+        "get_outer_html".to_owned()
+    );
+    assert_eq!(
+        field_name("removeXHRBreakpoint".to_owned()),
+        "remove_xhr_breakpoint".to_owned()
+    );
+    assert_eq!(
+        field_name("trackIndexedDBForOrigin".to_owned()),
+        "track_indexed_db_for_origin".to_owned()
+    );
+    assert_eq!(
+        field_name("trackIndexedDBForStorageKey".to_owned()),
+        "track_indexed_db_for_storage_key".to_owned()
+    );
+    assert_eq!(
+        field_name("printToPDF".to_owned()),
+        "print_to_pdf".to_owned()
+    );
+    assert_eq!(
+        field_name("setBypassCSP".to_owned()),
+        "set_bypass_csp".to_owned()
+    );
+    assert_eq!(
+        field_name("setCPUThrottlingRate".to_owned()),
+        "set_cpu_throttling_rate".to_owned()
+    );
+    assert_eq!(
+        field_name("setXHRBreakpoint".to_owned()),
+        "set_xhr_breakpoint".to_owned()
+    );
+    assert_eq!(
+        field_name("setShowFPSCounter".to_owned()),
+        "set_show_fps_counter".to_owned()
+    );
+    assert_eq!(
+        field_name("setOuterHTML".to_owned()),
+        "set_outer_html".to_owned()
+    );
+    assert_eq!(
+        field_name("setExtraHTTPHeaders".to_owned()),
+        "set_extra_http_headers".to_owned()
+    );
+    assert_eq!(
+        field_name("backendDOMNodeId".to_owned()),
+        "backend_dom_node_id".to_owned(),
+    );
+    assert_eq!(field_name("baseURL".to_owned()), "base_url".to_owned(),);
+    assert_eq!(
+        field_name("blockedURL".to_owned()),
+        "blocked_url".to_owned(),
+    );
+    assert_eq!(
+        field_name("currentSourceURL".to_owned()),
+        "current_source_url".to_owned(),
+    );
+    assert_eq!(field_name("dataURL".to_owned()), "data_url".to_owned(),);
+    assert_eq!(
+        field_name("documentURL".to_owned()),
+        "document_url".to_owned(),
+    );
+    assert_eq!(field_name("enableUI".to_owned()), "enable_ui".to_owned(),);
+    assert_eq!(
+        field_name("externalURL".to_owned()),
+        "external_url".to_owned(),
+    );
+    assert_eq!(
+        field_name("gatedAPIFeatures".to_owned()),
+        "gated_api_features".to_owned(),
+    );
+    assert_eq!(
+        field_name("hasSourceURL".to_owned()),
+        "has_source_url".to_owned(),
+    );
+    assert_eq!(
+        field_name("includeCommandLineAPI".to_owned()),
+        "include_command_line_api".to_owned(),
+    );
+    assert_eq!(
+        field_name("includeDOMRects".to_owned()),
+        "include_dom_rects".to_owned(),
+    );
+    assert_eq!(
+        field_name("includeObjectsCollectedByMajorGC".to_owned()),
+        "include_objects_collected_by_major_gc".to_owned(),
+    );
+    assert_eq!(
+        field_name("includeObjectsCollectedByMinorGC".to_owned()),
+        "include_objects_collected_by_minor_gc".to_owned(),
+    );
+    assert_eq!(
+        field_name("includeUserAgentShadowDOM".to_owned()),
+        "include_user_agent_shadow_dom".to_owned(),
+    );
+    assert_eq!(
+        field_name("initiatorIPAddressSpace".to_owned()),
+        "initiator_ip_address_space".to_owned(),
+    );
+    assert_eq!(
+        field_name("insecureURL".to_owned()),
+        "insecure_url".to_owned(),
+    );
+    assert_eq!(field_name("isBadUP".to_owned()), "is_bad_up".to_owned(),);
+    assert_eq!(field_name("isBadUV".to_owned()), "is_bad_uv".to_owned(),);
+    assert_eq!(field_name("isSVG".to_owned()), "is_svg".to_owned(),);
+    assert_eq!(
+        field_name("mainResourceURL".to_owned()),
+        "main_resource_url".to_owned(),
+    );
+    assert_eq!(
+        field_name("matchedCSSRules".to_owned()),
+        "matched_css_rules".to_owned(),
+    );
+    assert_eq!(field_name("modernSSL".to_owned()), "modern_ssl".to_owned(),);
+    assert_eq!(field_name("originURL".to_owned()), "origin_url".to_owned(),);
+    assert_eq!(field_name("outerHTML".to_owned()), "outer_html".to_owned(),);
+    assert_eq!(
+        field_name("preferCSSPageSize".to_owned()),
+        "prefer_css_page_size".to_owned(),
+    );
+    assert_eq!(
+        field_name("remoteIPAddress".to_owned()),
+        "remote_ip_address".to_owned(),
+    );
+    assert_eq!(
+        field_name("removeDOMBreakpoint".to_owned()),
+        "remove_dom_breakpoint".to_owned(),
+    );
+    assert_eq!(
+        field_name("removeDOMStorageItem".to_owned()),
+        "remove_dom_storage_item".to_owned(),
+    );
+    assert_eq!(field_name("reportAAA".to_owned()), "report_aaa".to_owned(),);
+    assert_eq!(
+        field_name("requestURL".to_owned()),
+        "request_url".to_owned(),
+    );
+    assert_eq!(
+        field_name("resourceIPAddressSpace".to_owned()),
+        "resource_ip_address_space".to_owned(),
+    );
+    assert_eq!(field_name("scopeURL".to_owned()), "scope_url".to_owned(),);
+    assert_eq!(field_name("scriptURL".to_owned()), "script_url".to_owned(),);
+    assert_eq!(
+        field_name("setDOMBreakpoint".to_owned()),
+        "set_dom_breakpoint".to_owned(),
+    );
+    assert_eq!(
+        field_name("setDOMStorageItem".to_owned()),
+        "set_dom_storage_item".to_owned(),
+    );
+    assert_eq!(field_name("showCSS".to_owned()), "show_css".to_owned(),);
+    assert_eq!(
+        field_name("sourceMapURL".to_owned()),
+        "source_map_url".to_owned(),
+    );
+    assert_eq!(field_name("sourceURL".to_owned()), "source_url".to_owned(),);
+    assert_eq!(
+        field_name("thresholdAA".to_owned()),
+        "threshold_aa".to_owned(),
+    );
+    assert_eq!(
+        field_name("thresholdAAA".to_owned()),
+        "threshold_aaa".to_owned(),
+    );
+    assert_eq!(
+        field_name("untrackIndexedDBForOrigin".to_owned()),
+        "untrack_indexed_db_for_origin".to_owned(),
+    );
+    assert_eq!(
+        field_name("untrackIndexedDBForStorageKey".to_owned()),
+        "untrack_indexed_db_for_storage_key".to_owned(),
+    );
+    assert_eq!(
+        field_name("userTypedURL".to_owned()),
+        "user_typed_url".to_owned(),
+    );
 }
